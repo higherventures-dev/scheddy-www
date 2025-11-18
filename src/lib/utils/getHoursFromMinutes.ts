60ed10d5a5f893165d05f50ee4e52ddf0b6457ce
actsdigital <jamal.ogans@acts.ventures>
2025-11-18 12:06:54 -0800

    Fix build issues and prepare marketing site for deployment

--- src/lib/utils/getHoursFromMinutes.ts
+++
@@ -1,8 +0,0 @@
-export default function getHoursFromMinutes(minutes: number): string {
-  const hrs = Math.floor(minutes / 60);
-  const mins = minutes % 60;
-
-  if (hrs && mins) return `${hrs} hours ${mins} mins`;
-  if (hrs) return `${hrs} hours`;
-  return `${mins} mins`;
-}
--- src/lib/utils/getReferralSource.ts
+++
@@ -1,8 +0,0 @@
-export function getReferralSource(referral_source: number): string {
-  switch (referral_source) {
-    case 1:
-      return "Walk-in";
-    default:
-      return "--";
-  }
-}
